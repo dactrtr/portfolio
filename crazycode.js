a5b00470479571417a31c10951d68427d55decb8
Dactrtr <sebastian.zuniga@elun.cl>
2020-08-13 23:29:42 -0400

    first animations

--- crazycode.js
+++ crazycode.js
@@ -34,15 +34,58 @@
 
 // Animations
 
-// gsap.to('.projects',{
-//   scrollTrigger: {
-//     trigger: '.briefcase',
-//     start: "top center",
-//     end:"center center",
-//     toggleActions:"restart pause reverse pause",
-//     markers: true,
-//     scrub: true,
-//   },
-//   y: -20,
-//   opacity: 1,
-// })
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from('.notros-screen',{
+  scrollTrigger: {
+    trigger: '.notros',
+    start: "top center",
+    end:"center center",
+    toggleActions:"restart pause reverse pause",
+    // markers: true,
+    scrub: true,
+  },
+  x: 200,
+  opacity: 0,
+});
+
+
+gsap.from('.notros',{
+  scrollTrigger: {
+    trigger: '.what',
+    start: "top bottom",
+    end:"top top",
+    toggleActions:"restart pause reverse pause",
+    // markers: true,
+    scrub: true,
+  },
+  y: 200,
+  opacity: 0.7,
+});
+
+gsap.from('.mck',{
+  scrollTrigger: {
+    trigger: '.notros',
+    start: "bottom bottom",
+    end:"bottom center",
+    toggleActions:"restart pause reverse pause",
+    // markers: true,
+    scrub: true,
+  },
+  y: 200,
+  opacity: 0.7,
+});
+// let sections = gsap.utils.toArray(".brief-container");
+
+// gsap.to(sections, {
+//   yPercent: -100 * (sections.length -1),
+//   ease: "none",
+//   scrollTrigger:{
+//     trigger: ".what",
+//     pin: true,
+//     scrub: 1,
+//     snap: 1/ (sections.length -1),
+//     end: () => "+=" + 
+//     document.querySelector(".projects").offsetHeight
+//   }
+// });
